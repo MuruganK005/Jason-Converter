97a177523338979d988ddc6363b51c25c93a4fa3
DESKTOP-NHDE5NR\Murugan K <muruganeee005@gmail.com>
2022-06-22 11:21:15 +0530

    ujuj

--- src/main/java/com/jsonConvert/Converting_Json/Controller/JSONToXMLMappingController.java
+++ src/main/java/com/jsonConvert/Converting_Json/Controller/JSONToXMLMappingController.java
@@ -1,15 +1,15 @@
 
 package com.jsonConvert.Converting_Json.Controller;
 
+import com.github.underscore.U;
 import com.jsonConvert.Converting_Json.Model.RequestModel;
+import org.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.json.XML;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class JSONToXMLMappingController {
 
     @PostMapping(path = "/mapJsontoXML", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_XML_VALUE)
@@ -18,8 +18,8 @@ public class JSONToXMLMappingController {
         return request;
     }
 
-    @PostMapping(path = "/mapXMLtoJson", consumes = MediaType.APPLICATION_XML_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody String mapXMLtoJson(@RequestBody final RequestModel request) {
+    @PostMapping("/mapXMLtoJson")
+    public  String mapXMLtoJson() {
          String XML_TEXT = "<note>\n" +
                 "<to>Tove</to>\n" +
                 "<from>Jani</from>\n" +
@@ -29,4 +29,17 @@ public class JSONToXMLMappingController {
 //        return request;
         return XML.toJSONObject(XML_TEXT).toString();
     }
+    @PostMapping(path = "/mapXMLtoJson1", consumes = MediaType.APPLICATION_XML_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public RequestModel mapXMLtoJson1(@RequestBody RequestModel request) {
+        return request;
+    }
+    @PostMapping("/mapJsonToXml1")
+    public  String mapJsonToXml1() {
+        String json = "{\"name\":\"JSON\",\"integer\":1,\"double\":2.0,\"boolean\":true,\"nested\": {\"id\":42},"
+                + "\"array\":[1,2,3]}";
+        String xml = U.jsonToXml(json);
+        U.xmlToJson(xml);
+        return xml;
+    }
+
 }
--- src/main/java/com/jsonConvert/Converting_Json/ServletInitializer.java
+++ src/main/java/com/jsonConvert/Converting_Json/ServletInitializer.java
@@ -1,2 +1,14 @@
-package com.jsonConvert.Converting_Json;public class ServletInitializer {
+
+package com.jsonConvert.Converting_Json;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ConvertingJsonApplication.class);
+    }
+
 }
